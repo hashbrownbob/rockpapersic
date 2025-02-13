a18df1aff699d01b428fde27037d7c5dd511b977
hashbrownbob <alexander.hake@outlook.com>
2025-02-13 16:34:46 +0000

    finished

--- dcz.py
+++ dcz.py
@@ -1,1 +1,43 @@
+import random
+
+def get_player_choice():
+        player_choice = input("Enter 'rock', 'paper', or 'scissors': ").lower()
+        if player_choice in ['rock', 'paper', 'scissors']:
+            return player_choice
+        else:
+            print("huh?.")
+
+def get_computer_choice():
+    return random.choice(['rock', 'paper', 'scissors'])
+
+def determine_winner(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        return "you wim!"
+    elif (player_choice == 'rock' and computer_choice == 'scissors') or \
+         (player_choice == 'scissors' and computer_choice == 'paper') or \
+         (player_choice == 'paper' and computer_choice == 'rock'):
+        return "You win!"
+    else:
+        return "You lose!"
+
+def play_game():
+    print("Welcome to Rock, Paper, Scissors!")
+    
+    while True:
+        player_choice = get_player_choice()
+        computer_choice = get_computer_choice()
+        
+        print(f"You chose: {player_choice}")
+        print(f"The computer chose: {computer_choice}")
+        
+        result = determine_winner(player_choice, computer_choice)
+        print(result)
+        
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
+            print("Thanks for playing!")
+            break
+
+if __name__ == "__main__":
+    play_game()
 
